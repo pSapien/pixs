20e4f41c498c474981b8bebe2cedf60849f6a8c4
Prateek Thapa <prateekthapa26@gmail.com>
2019-07-09 07:30:19 +0545

    added border and padding

--- lib/src/widgets/image_list.dart
+++ lib/src/widgets/image_list.dart
@@ -11,7 +11,14 @@ class ImageList extends StatelessWidget {
     return ListView.builder(
       itemCount: images.length,
       itemBuilder: (BuildContext context, int id) {
-        return Text(images[id].url);
+        return Container(
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey),
+          ),
+          padding: EdgeInsets.all(20.0),
+          margin: EdgeInsets.all(20.0),
+          child: Image.network(images[id].url),
+        );
       },
     );
   }
